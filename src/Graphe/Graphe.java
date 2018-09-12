4bd61342f788ae8a7b9c0ddbc3ac166bccb2632f
Epulapp <Epulapp@epulapp111.univ-lyon1.fr>
2018-09-12 18:00:27 +0200

    first commit

---
+++ src/Graphe/Graphe.java
@@ -0,0 +1,8 @@
+package Graphe;
+
+import java.util.ArrayList;
+
+public class Graphe extends ArrayList<Noeud> {
+
+
+}
---
+++ src/Graphe/Noeud.java
@@ -0,0 +1,8 @@
+package Graphe;
+
+import java.util.HashMap;
+
+public class Noeud {
+
+    HashMap<Integer, Noeud> voisins;
+}
